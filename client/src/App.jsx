771093944ecdf5e04b9cb26f0b19a206dac6ceba
Lakshya-kesarwani <167260069+Lakshya-kesarwani@users.noreply.github.com>
2025-03-29 16:52:32 +0000

    upoladed assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import BuyCredits from './pages/BuyCredits'
 import Navbar from './components/Navbar'
 const App = () => {
   return (
-    <div>
+    <div className='min-h-screen bg-slate-50'>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
