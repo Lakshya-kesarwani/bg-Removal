e34cb7d54bcd8825234091e9631dd2db08b2716b
Lakshya-kesarwani <167260069+Lakshya-kesarwani@users.noreply.github.com>
2025-03-30 16:26:57 +0000

    server setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+//App config
+const PORT = process.env.PORT || 4000
+const app = express()   
+
+// Intialize Middlewares
+app.use(express.json())
+app.use(cors())
+
+// API routes
+app.get('/', (req, res) => res.send('Api working'))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
