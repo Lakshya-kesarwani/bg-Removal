50516c7a5b2201ff52f9f8b5cd9f044dfdb24988
Lakshya-kesarwani <167260069+Lakshya-kesarwani@users.noreply.github.com>
2025-03-30 15:52:20 +0000

    authentication added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ClerkProvider } from '@clerk/clerk-react'
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-
-    <App />
+  <BrowserRouter >
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} >
+      <App />
+    </ClerkProvider>
   </BrowserRouter>,
 )
